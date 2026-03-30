a661f0051a1098f126e29718b07c2fccd6ad0473
PedroHenriqueAlvesDias <pedrohadias123@gmail.com>
2026-03-30 12:41:37 -0300

    Update Form1.cs
    Updated comments for clarity and improved validation messages.

--- IMC_APP/Form1.cs
+++ IMC_APP/Form1.cs
@@ -9,32 +9,35 @@ namespace WinFormsApp1
 
         private void btnVerificar_Click(object sender, EventArgs e)
         {
+            //Declaração das variavéis
             decimal alt, pes, imc;
 
-            // Limpa lista
+            // Limpa lista(para nao ficar adicionando infinito)
             lstVerificar.Items.Clear();
-
-            // Validação altura
+            
+            //Se NÃO for possível converter o texto de txtAltura para decimal(caso consiga, armazene na variavel alt).
             if (!decimal.TryParse(txtAltura.Text, out alt))
             {
+                //então mostre ‘Altura inválida!’ e pare a execução.
                 lstVerificar.Items.Add("Altura inválida!");
                 return;
             }
 
-            // Validação peso
+            //Se NÃO for possível converter o texto de txtPeso para decimal(caso consiga, armazene na variavel pes).
             if (!decimal.TryParse(txtPeso.Text, out pes))
             {
+                //então mostre ‘Peso inválido!’ e pare a execução.
                 lstVerificar.Items.Add("Peso inválido!");
                 return;
             }
 
-            // Converter cm para metros (se necessário)
+            // Converção: cm para metros (se necessário)
             if (alt > 3)
             {
                 alt = alt / 100;
             }
 
-            // Validação de valores absurdos
+            // Validação de valores irreais
             if (alt <= 0 || pes <= 0)
             {
                 lstVerificar.Items.Add("Valores devem ser maiores que zero!");
@@ -46,7 +49,6 @@ namespace WinFormsApp1
 
             // Classificação
             string clsf;
-
             if (imc < 18.5m)
                 clsf = "Magreza";
             else if (imc < 25m)
@@ -58,7 +60,7 @@ namespace WinFormsApp1
             else
                 clsf = "Obesidade grave";
 
-            // Exibição
+            // Exibição dos valores inseridos
             lstVerificar.Items.Add("Altura: " + alt.ToString("F2") + " m");
             lstVerificar.Items.Add("Peso: " + pes + " kg");
             lstVerificar.Items.Add("IMC: " + imc.ToString("F2"));
